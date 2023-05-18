f859682c67b63bd41f37acfa634a72f4f7d9ac39
ravibootlabs <109270469+ravibootlabs@users.noreply.github.com>
2023-05-18 12:29:59 +0530

    changes the permission

--- main.tf
+++ main.tf
@@ -11,4 +11,11 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.main.id
   acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.main.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
 }
